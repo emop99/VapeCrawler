90603cda28dd0b0ffdcedccf0da52e2735890653
유희 <emop99@naver.com>
2026-01-07 22:11:10 +0900

    Add JuicemarketCrawler implementation and update imports in related files

--- crawlers/__init__.py
+++ crawlers/__init__.py
@@ -30,5 +30,6 @@ from .everyonevape_crawler import EveryoneVapeCrawler
 from .ciganuri_crawler import CiganuriCrawler
 from .tfnmall_crawler import TfnmallCrawler
 from .aecsangdeokhu_crawler import AecsangdeokhuCrawler
+from .juicemarket_crawler import JuicemarketCrawler
 
-__all__ = ['BaseCrawler', 'VapeMonsterCrawler', 'VapingLabCrawler', 'Juice24Crawler', 'Juice99Crawler', 'JuiceboxCrawler', 'JuiceshopCrawler', 'SkyVapeCrawler', 'KimiVapeCrawler', 'JuicegramCrawler', 'Vape49Crawler', 'LoungeVapeCrawler', 'Juice79Crawler', 'BreathingKoreaCrawler', 'Vape9Crawler', 'VapeingduckCrawler', 'VapebibiCrawler', 'VaporwaveCrawler', 'WitchjuiceCrawler', 'MamavapeCrawler', 'Vape365Crawler', 'Juice23Crawler', 'KarivapeCrawler', 'EveryoneVapeCrawler', 'CiganuriCrawler', 'TfnmallCrawler', 'AecsangdeokhuCrawler']
+__all__ = ['BaseCrawler', 'VapeMonsterCrawler', 'VapingLabCrawler', 'Juice24Crawler', 'Juice99Crawler', 'JuiceboxCrawler', 'JuiceshopCrawler', 'SkyVapeCrawler', 'KimiVapeCrawler', 'JuicegramCrawler', 'Vape49Crawler', 'LoungeVapeCrawler', 'Juice79Crawler', 'BreathingKoreaCrawler', 'Vape9Crawler', 'VapeingduckCrawler', 'VapebibiCrawler', 'VaporwaveCrawler', 'WitchjuiceCrawler', 'MamavapeCrawler', 'Vape365Crawler', 'Juice23Crawler', 'KarivapeCrawler', 'EveryoneVapeCrawler', 'CiganuriCrawler', 'TfnmallCrawler', 'AecsangdeokhuCrawler', 'JuicemarketCrawler']
